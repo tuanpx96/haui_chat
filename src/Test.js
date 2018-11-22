245daf61950385c86c7531d630a9d9f5221ac6ec
tuanpx96 <tuanpx96@gmail.com>
2018-11-22 17:31:59 +0700

    create index

---
+++ src/Test.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Test extends Component {
+    render() {
+        return (
+            <div className="Test">
+
+                <p>
+                    Edit <code>src/App.js</code> ahiahi
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Test;
